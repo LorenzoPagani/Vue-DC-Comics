aa62d64bf9f6ec3ee247ec1eb914e05443dca1ef
Lorenzo Pagani <larrypg2@gmail.com>
2023-11-29 17:20:30 +0100

    feat:adds call to action component

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@ import appHeader from './components/appHeader.vue'
 import appContent from './components/appContent.vue'
 import buyComics from './components/buyComics.vue'
 import footerMenu from './components/footerMenu.vue'
+import callToAction from './components/callToAction.vue'
 export default {
   components: {
     appHeader,
     appContent,
     buyComics,
     footerMenu,
+    callToAction
   }
 }
 </script>
@@ -21,6 +23,7 @@ export default {
   </main>
   <footer>
     <footerMenu />
+    <callToAction />
   </footer>
 </template>
 
---
+++ src/components/callToAction.vue
@@ -0,0 +1,27 @@
+<script></script>
+<template>
+    <div class="wrapper">
+        <div class="container h-100">
+            <div class="row h-100">
+                <div class="col-6 d-flex align-items-center  h-100">
+                    <button type="button" class="btn btn-outline-primary text-white">SIGN-UP NOW!</button>
+                </div>
+                <div class="col-6 d-flex justify-content-end align-items-center gap-2">
+                    <span class="text-primary">FOLLOW US</span>
+                    <i class="fa-brands fa-facebook fa-lg text-secondary"></i>
+                    <i class="fa-brands fa-twitter fa-lg text-secondary"></i>
+                    <i class="fa-brands fa-youtube fa-lg text-secondary"></i>
+                    <i class="fa-brands fa-pinterest fa-lg text-secondary"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style>
+.wrapper {
+    height: 15vh;
+    background-color: #303030;
+    position: relative;
+    z-index: 1;
+}
+</style>
